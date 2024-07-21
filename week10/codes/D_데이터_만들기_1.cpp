f98016864d0e4080ade654ad0acd6d13341a889b
유현서 <vmffotltka1@gmail.com>
2024-07-21 20:55:21 +0900

    Create D_데이터_만들기_1.cpp

---
+++ week10/codes/D_데이터_만들기_1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cout << "103\n";
+    for (int i = 0; i < 103; i++)
+        cout << "0\n";
+    cout << "1\n0 1";
+}
